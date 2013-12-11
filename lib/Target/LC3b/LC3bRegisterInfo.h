fb5506655a83270798c11a23b0d6ce5ae71b9238
joyjoyboy <yuwenxiao.zju@gmail.com>
2013-12-11 04:05:24 -0600

    Update LC3bRegisterInfo.h
    Constructor and getReservedRegs() are kept declared.

--- lib/Target/LC3b/LC3bRegisterInfo.h
+++ lib/Target/LC3b/LC3bRegisterInfo.h
@@ -16,6 +16,8 @@
 #ifndef LC3BREGISTERINFO_H
 #define LC3BREGISTERINFO_H
 
+#include "LC3b.h"
+#include "llvm/Target/TargetRegisterInfo.h"
 #include "llvm/ADT/BitVector.h"
 
 #define GET_REGINFO_HEADER
@@ -30,21 +32,19 @@ class LC3bRegisterInfo : public LC3bGenRegisterInfo
    LC3bTargetMachine& TM;
 
 public:
-   LC3bRegisterInfo(LC3bTargetMachine& tm)
-    : LC3bGenRegisterInfo(0), // FIXME: What parameters to pass here?
-      TM(tm)
-   {}
+   // Defined in .cpp file
+   LC3bRegisterInfo(LC3bTargetMachine& tm);
 
    // Implement pure virtual functions from TargetRegisterInfo
-   const uint16_t* getCalleeSavedRegs(const MachineFunction* MF = 0) const;
+//   const uint16_t* getCalleeSavedRegs(const MachineFunction* MF = 0) const;
   
    BitVector getReservedRegs(const MachineFunction& MF) const;
 
-   void eliminateFrameIndex(MachineBasicBlock::iterator MI,
-                            int SPAdj, unsigned FIOperandNum,
-                            RegScavenger *RS = NULL) const;
+//   void eliminateFrameIndex(MachineBasicBlock::iterator MI,
+//                            int SPAdj, unsigned FIOperandNum,
+//                            RegScavenger *RS = NULL) const;
 
-   unsigned getFrameRegister(const MachineFunction &MF) const;
+//   unsigned getFrameRegister(const MachineFunction &MF) const;
 };
 
 }
